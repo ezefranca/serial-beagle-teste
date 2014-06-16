f2a0d834850b6b9f2572c9c68f41f8c14c0f71ee
Ezequiel <ezequiel.ifsp@gmail.com>
2014-06-16 15:22:43 -0300

    classes iniciais

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Arduino.h"
+#include "SerialStream.h"
+#include <SerialStreamBuf.h>
+#include <SerialPort.h>
+#include <string>
+
+using namespace std;
+using namespace LibSerial;
+
+int main(int argc, char **argv)
+{
+    Arduino arduino();
+    return 0;
+}
